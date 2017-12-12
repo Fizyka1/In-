daf38f152ceaa46f4cf83b99deb97622cbc732c4
MATEUSZ <kiczor8@vp.pl>
2017-12-12 15:48:38 +0100

    start files

--- .idea/chain.py
+++ .idea/chain.py
@@ -23,108 +23,108 @@ class Chain(object):
             if pdb<=probability[0]:
                 #pierwszy atom chce się ruszyć do góry w lewo
                 if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]+=1
-                elif self.yLattice[atom_number]+1==self.yLattice[atom_number+1] and self.xLattice[atom_number]-1==self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]+=1
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]+=self.move
+                elif self.yLattice[atom_number]+self.move==self.yLattice[atom_number+1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number+1]:
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]+=self.move
             elif pdb>probability[0] and pdb<=2*probability[0]:
                 #pierwszy atom chce się ruszyć do gó©y w prawo
                 if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number]+=1
-                    self.yLattice[atom_number]+=1
-                elif self.yLattice[atom_number]+1==self.yLattice[atom_number+1] and self.xLattice[atom_number]+1==self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number]+=1
-                    self.yLattice[atom_number]+=1
+                    self.xLattice[atom_number]+=self.move
+                    self.yLattice[atom_number]+=self.move
+                elif self.yLattice[atom_number]+self.move==self.yLattice[atom_number+1] and self.xLattice[atom_number]+self.move==self.xLattice[atom_number+1]:
+                    self.xLattice[atom_number]+=self.move
+                    self.yLattice[atom_number]+=self.move
             elif pdb>2*probability[0] and pdb<=(2*probability[0]+probability[1]):
                 #pierwszy atom chce się ruszyć do dołu w lewo
                 if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]-=1
-                elif self.yLattice[atom_number]-1==self.yLattice[atom_number+1] and self.xLattice[atom_number]-1==self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]-=1
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]-=self.move
+                elif self.yLattice[atom_number]-self.move==self.yLattice[atom_number+1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number+1]:
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]-=self.move
             else:
                 #pierwszy atom chce się ruszyć do tyłu w prawo
                 if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number] += 1
-                    self.yLattice[atom_number] -= 1
-                elif self.yLattice[atom_number]-1 == self.yLattice[atom_number+1] and self.xLattice[atom_number]+1 == self.xLattice[atom_number+1]:
-                    self.xLattice[atom_number] += 1
-                    self.yLattice[atom_number] -= 1
+                    self.xLattice[atom_number] += self.move
+                    self.yLattice[atom_number] -= self.move
+                elif self.yLattice[atom_number]-self.move == self.yLattice[atom_number+1] and self.xLattice[atom_number]+self.move == self.xLattice[atom_number+1]:
+                    self.xLattice[atom_number] += self.move
+                    self.yLattice[atom_number] -= self.move
 
         elif atom_number==self.chain_len-1:
             #Kiedy pierwszy atom łańcucha może poruszyć się w każdą stronę
             if pdb<=probability[0]:
                 #pierwszy atom chce się ruszyć do góry w lewo
                 if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]+=1
-                elif self.yLattice[atom_number]+1==self.yLattice[atom_number-1] and self.xLattice[atom_number]-1==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]+=1
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]+=self.move
+                elif self.yLattice[atom_number]+self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number-1]:
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]+=self.move
             elif pdb>probability[0] and pdb<=2*probability[0]:
                 #pierwszy atom chce się ruszyć do gó©y w prawo
                 if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]+=1
-                    self.yLattice[atom_number]+=1
-                elif self.yLattice[atom_number]+1==self.yLattice[atom_number-1] and self.xLattice[atom_number]+1==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]+=1
-                    self.yLattice[atom_number]+=1
+                    self.xLattice[atom_number]+=self.move
+                    self.yLattice[atom_number]+=self.move
+                elif self.yLattice[atom_number]+self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]+self.move==self.xLattice[atom_number-1]:
+                    self.xLattice[atom_number]+=self.move
+                    self.yLattice[atom_number]+=self.move
             elif pdb>2*probability[0] and pdb<=(2*probability[0]+probability[1]):
                 #pierwszy atom chce się ruszyć do dołu w lewo
                 if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]-=1
-                elif self.yLattice[atom_number]-1==self.yLattice[atom_number-1] and self.xLattice[atom_number]-1==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]-=1
-                    self.yLattice[atom_number]-=1
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]-=self.move
+                elif self.yLattice[atom_number]-self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number-1]:
+                    self.xLattice[atom_number]-=self.move
+                    self.yLattice[atom_number]-=self.move
             else:
                 #pierwszy atom chce się ruszyć do tyłu w prawo
                 if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]+=1
-                    self.yLattice[atom_number]-=1
-                elif self.yLattice[atom_number]-1==self.yLattice[atom_number-1] and self.xLattice[atom_number]+1==self.xLattice[atom_number-1]:
-                    self.xLattice[atom_number]+=1
-                    self.yLattice[atom_number]-=1
+                    self.xLattice[atom_number]+=self.move
+                    self.yLattice[atom_number]-=self.move
+                elif self.yLattice[atom_number]-self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]+self.move==self.xLattice[atom_number-1]:
+                    self.xLattice[atom_number]+=self.move
+                    self.yLattice[atom_number]-=self.move
         #sprawdzanie możliwości ruchu pozostałych atomów w łańcuchu
         else:
             if pdb<=probability[0]:
-                if self.yLattice[atom_number]+1==self.yLattice[atom_number+1] and self.xLattice[atom_number]-1==self.xLattice[atom_number+1]:
+                if self.yLattice[atom_number]+self.move==self.yLattice[atom_number+1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number+1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                        self.yLattice[atom_number]+=1
-                        self.xLattice[atom_number]-=1
-                if self.yLattice[atom_number]+1==self.yLattice[atom_number-1] and self.xLattice[atom_number]-1==self.xLattice[atom_number-1]:
+                        self.yLattice[atom_number]+=self.move
+                        self.xLattice[atom_number]-=self.move
+                if self.yLattice[atom_number]+self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number-1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                        self.yLattice[atom_number]+=1
-                        self.xLattice[atom_number]-=1
+                        self.yLattice[atom_number]+=self.move
+                        self.xLattice[atom_number]-=self.move
             elif pdb>probability[0] and pdb<=2*probability[0]:
-                if self.yLattice[atom_number]+1==self.yLattice[atom_number+1] and self.xLattice[atom_number]+1==self.xLattice[atom_number+1]:
+                if self.yLattice[atom_number]+self.move==self.yLattice[atom_number+1] and self.xLattice[atom_number]+self.move==self.xLattice[atom_number+1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                        self.yLattice[atom_number]+=1
-                        self.xLattice[atom_number]+=1
-                if self.yLattice[atom_number]+1==self.yLattice[atom_number-1] and self.xLattice[atom_number]+1==self.xLattice[atom_number-1]:
+                        self.yLattice[atom_number]+=self.move
+                        self.xLattice[atom_number]+=self.move
+                if self.yLattice[atom_number]+self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]+self.move==self.xLattice[atom_number-1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                        self.yLattice[atom_number]+=1
-                        self.xLattice[atom_number]+=1
+                        self.yLattice[atom_number]+=self.move
+                        self.xLattice[atom_number]+=self.move
             elif pdb>2*probability[0] and pdb<=(2*probability[0]+probability[1]):
-                if self.yLattice[atom_number]-1==self.yLattice[atom_number+1] and self.xLattice[atom_number]-1==self.xLattice[atom_number+1]:
+                if self.yLattice[atom_number]-self.move==self.yLattice[atom_number+1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number+1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                        self.yLattice[atom_number]-=1
-                        self.xLattice[atom_number]-=1
-                if self.yLattice[atom_number]-1==self.yLattice[atom_number-1] and self.xLattice[atom_number]-1==self.xLattice[atom_number-1]:
+                        self.yLattice[atom_number]-=self.move
+                        self.xLattice[atom_number]-=self.move
+                if self.yLattice[atom_number]-self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]-self.move==self.xLattice[atom_number-1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                        self.yLattice[atom_number]-=1
-                        self.xLattice[atom_number]-=1
+                        self.yLattice[atom_number]-=self.move
+                        self.xLattice[atom_number]-=self.move
             else:
-                if self.yLattice[atom_number]-1==self.yLattice[atom_number+1] and self.xLattice[atom_number]+1==self.xLattice[atom_number+1]:
+                if self.yLattice[atom_number]-self.move==self.yLattice[atom_number+1] and self.xLattice[atom_number]+self.move==self.xLattice[atom_number+1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number-1] and self.xLattice[atom_number]==self.xLattice[atom_number-1]:
-                        self.yLattice[atom_number]-=1
-                        self.xLattice[atom_number]+=1
-                if self.yLattice[atom_number]-1==self.yLattice[atom_number-1] and self.xLattice[atom_number]+1==self.xLattice[atom_number-1]:
+                        self.yLattice[atom_number]-=self.move
+                        self.xLattice[atom_number]+=self.move
+                if self.yLattice[atom_number]-self.move==self.yLattice[atom_number-1] and self.xLattice[atom_number]+self.move==self.xLattice[atom_number-1]:
                     if self.yLattice[atom_number]==self.yLattice[atom_number+1] and self.xLattice[atom_number]==self.xLattice[atom_number+1]:
-                        self.yLattice[atom_number]-=1
-                        self.xLattice[atom_number]+=1
+                        self.yLattice[atom_number]-=self.move
+                        self.xLattice[atom_number]+=self.move
     def reset(self):
         self.xLattice = [10.5 for x in range(self.chain_len)]
         self.yLattice = [0.5 for x in range(self.chain_len)]
