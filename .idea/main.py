c2e721b9aedc6ca8fab0d96ec4eb08f1d56be6b7
MATEUSZ <kiczor8@vp.pl>
2018-01-13 17:30:57 +0100

    all works

--- .idea/main.py
+++ .idea/main.py
@@ -11,13 +11,14 @@ random.seed(a=None)
 root = Tk()
 
 #Init, creating class objects
-
 params = Params()
 user_interface = GUI(root,params)
 
+#creating chains
 chain1 = Chain(user_interface,params.chain1_len,color="gray")
 chain2 = Chain(user_interface,params.chain2_len,color="limegreen",second=True)
 
+#drawing chains
 chain1.draw_chain()
 chain2.draw_chain()
 
@@ -31,5 +32,6 @@ def RESET():
 
 button_reset = Button(user_interface.frame_left_up_lower,image=user_interface.reset_png,command=RESET,width=user_interface.button_size,height=user_interface.button_size)
 button_reset.pack(side='top')
+
 #starting window mainloop
 root.mainloop()
